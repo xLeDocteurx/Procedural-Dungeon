cd40b2a9cebbdf15754b3bcdbcb824bbfe5ef470
acerveaux <acerveaux@botdesign.net>
2020-10-10 18:04:37 +0200

    Ordered of exported scenes.

--- src/scripts/scenes/index.ts
+++ src/scripts/scenes/index.ts
@@ -1,2 +1,10 @@
-export { default as MainScene } from './main/main.scene'
-export { default as PreloadScene } from './preload/preload.scene'
+/*
+ *  scenes `index` module
+ *  =====================
+ *
+ *  Declares all present game scenes.
+ *  Expose the required game scenes using this module.
+ *  Tips : The preload scene and always first
+ */
+export * from './preload/preload.scene'
+export * from './main/main.scene'
